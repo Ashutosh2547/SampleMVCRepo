c5e9ef1a922508d1cc894b5f496a926fbfe3a985
NAGARRO\ashutosh2547 <ashutosh.tripathi@nagarro.com>
2018-02-13 02:14:07 +0530

    first commit

---
+++ BusinessLayer/SampleBDC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class SampleBDC
+    {
+        public string GetApplicationHeading()
+        {
+            return ".NET MVC WebApplication ts";
+        }
+    }
+}
---
+++ BusinessLayerTests/SampleBDCTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLayer;
+
+namespace BusinessLayerUnitTest
+{
+    [TestClass]
+    public class SampleBDCTest
+    {
+        [TestMethod]
+        public void GetApplicationHeading_ForEveryCall_ReturnsDummyValue()
+        {
+            #region Arrange
+
+            SampleBDC sampleBDC = new SampleBDC();
+
+            #endregion Arrange
+
+
+            #region Act & Assert
+            var heading = sampleBDC.GetApplicationHeading();
+
+            Assert.AreEqual(heading, ".NET MVC WebApplication ts");
+
+            #endregion Act & Assert
+        }
+    }
+}
